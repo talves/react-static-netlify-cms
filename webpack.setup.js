e8adfdc98d9c886eb5dbec4101ab26aa9886f2ad
talves <tony@3alves.com>
2017-12-10 17:20:51 -0800

    try this one

--- webpack.setup.js
+++ webpack.setup.js
@@ -4,38 +4,53 @@ const SASS_INCLUDE_PATHS = ['src/']
 const extractSass = new ExtractTextPlugin({
   filename: 'styles.[hash:8].css',
   disable: process.env.NODE_ENV === 'development',
+  allChunks: true,
 })
 let alreadyIn = false
+let sassUse
+if (process.env.NODE_ENV === 'development') {
+  sassUse = [{
+    loader: 'style-loader', // creates style nodes from JS strings
+  }, {
+    loader: 'css-loader', // translates CSS into CommonJS
+  }, {
+    loader: 'sass-loader', // compiles Sass to CSS
+  }]
+} else {
+  sassUse = ExtractTextPlugin.extract({
+    use: [
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          minimize: true,
+          sourceMap: false,
+        },
+      },
+      {
+        loader: 'sass-loader',
+        options: { includePaths: SASS_INCLUDE_PATHS },
+      },
+    ],
+  })
+}
 
 export default (config, { stage, defaultLoaders }) => {
+  // react-static is loading this config twice, but not needed
   if (!alreadyIn) { alreadyIn = true } else { return config }
-  console.log('extractSass-', extractSass)
   config.module.rules = [{
     oneOf: [
       {
         test: /\.s(a|c)ss$/,
-        use: extractSass.extract({
-          // use style-loader in development
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                minimize: stage === 'prod',
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: { includePaths: SASS_INCLUDE_PATHS },
-            },
-          ],
-        }),
+        use: sassUse,
       },
-      defaultLoaders.jsLoader,
       defaultLoaders.cssLoader,
+      defaultLoaders.jsLoader,
+    ],
+    rules: [
       defaultLoaders.fileLoader,
     ],
   }]
-  config.plugins.push(extractSass)
+  if (stage === 'dev') config.plugins.push(extractSass)
   return config
 }
