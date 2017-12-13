6fb37ead347af3e401e23467f6d7e74f3eb5c0cf
talves <tony@3alves.com>
2017-12-12 17:56:56 -0800

    use stage instead

--- webpack.setup.js
+++ webpack.setup.js
@@ -2,41 +2,44 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 const SASS_INCLUDE_PATHS = ['src/', 'node_modules/']
 
-let sassUse = []
-if (process.env.NODE_ENV === 'development') {
-  sassUse = [
-    { loader: 'style-loader' },
-    { loader: 'css-loader' },
-    {
-      loader: 'sass-loader',
-      options: { includePaths: SASS_INCLUDE_PATHS },
-    },
-  ]
-} else {
-  sassUse = ExtractTextPlugin.extract({
-    use: [
-      {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
-          minimize: true,
-          sourceMap: false,
-        },
-      },
+const sassUse = stage => {
+  let usecase = []
+  if (stage === 'dev') {
+    usecase = [
+      { loader: 'style-loader' },
+      { loader: 'css-loader' },
       {
         loader: 'sass-loader',
         options: { includePaths: SASS_INCLUDE_PATHS },
       },
-    ],
-  })
+    ]
+  } else {
+    usecase = ExtractTextPlugin.extract({
+      use: [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            minimize: true,
+            sourceMap: false,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: { includePaths: SASS_INCLUDE_PATHS },
+        },
+      ],
+    })
+  }
+  return usecase
 }
 
-export default (config, { defaultLoaders }) => {
+export default (config, { defaultLoaders, stage }) => {
   config.module.rules = [{
     oneOf: [
       {
         test: /\.s(a|c)ss$/,
-        use: sassUse,
+        use: sassUse(stage),
       },
       defaultLoaders.cssLoader,
       defaultLoaders.jsLoader,
