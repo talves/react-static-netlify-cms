5240baa899d4d141c7bb880dde2a3c81bcbe901d
talves <tony@3alves.com>
2018-03-11 11:57:51 -0700

    🔗 quick links

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -16,17 +16,29 @@ export default withSiteData(() => (
     <GridCell span="8"><Typography tag="h1" style={{ textAlign: 'center' }} use="display2">Welcome to</Typography></GridCell>
     <GridCell span="2" />
     <GridCell span="2" />
-    <GridCell span="8"><img className="nc-Page-logo" src={logoCMSImg} alt="Netlify CMS" /></GridCell>
-    <GridCell span="2" />
-    <GridCell span="2" />
-    <GridCell span="8"><img style={Object.assign({}, { height: 'auto', width: '100%' })} src={logoImg} alt="" /></GridCell>
+    <GridCell span="8">
+      <a href="https://www.netlifycms.org/" target="_blank" rel="noopener noreferrer">
+        <img className="nc-Page-logo" src={logoCMSImg} alt="Netlify CMS" />
+      </a>
+    </GridCell>
     <GridCell span="2" />
     <GridCell span="2" />
     <GridCell span="8">
-      <Elevation z="8">
-        <Typography tag="h1" style={{ textAlign: 'center' }} use="display2"><img style={Object.assign({}, { height: 'auto', width: '2.0em' })} src={logoMDCImg} alt="" /> Using Material Design Web Components</Typography>
-        <Typography tag="div" style={{ textAlign: 'center' }} use="headline">RMWC - React Material Web Components - by James Friedman</Typography>
-      </Elevation>
+      <a href="https://github.com/nozzle/react-static" target="_blank" rel="noopener noreferrer">
+        <img style={Object.assign({}, { height: 'auto', width: '100%' })} src={logoImg} alt="" />
+      </a>
+    </GridCell>
+    <GridCell span="2" />
+    <GridCell span="2" />
+    <GridCell span="8">
+      <a href="https://github.com/nozzle/react-static" target="_blank" rel="noopener noreferrer">
+        <Elevation z="8">
+          <Typography tag="h1" style={{ textAlign: 'center' }} use="display2">
+            <img style={Object.assign({}, { height: 'auto', width: '2.0em' })} src={logoMDCImg} alt="" /> Using Material Design Web Components
+          </Typography>
+          <Typography tag="div" style={{ textAlign: 'center' }} use="headline">RMWC - React Material Web Components - by James Friedman</Typography>
+        </Elevation>
+      </a>
     </GridCell>
     <GridCell span="2" />
   </Grid>
